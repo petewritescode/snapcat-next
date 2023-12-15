4379971fa66607f9e5a7200f9b6758bd1a062daf
Pete Williams <hello@petewritescode.com>
2023-12-15 21:38:39 +0000

    Render images from API

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,20 @@
+import { getImages } from '@/api/get-images';
+import NextImage from 'next/image';
 import { FC } from 'react';
 
-const HomePage: FC = () => <>Home page</>;
+const HomePage: FC = async () => {
+  const images = await getImages();
+
+  return images.map((image) => (
+    <div key={image.id}>
+      <NextImage
+        src={image.url}
+        alt="Cat image"
+        width={image.width}
+        height={image.height}
+      />
+    </div>
+  ));
+};
 
 export default HomePage;
