9c4687b8cedec9b090ed29e37e500c522491182c
Pete Williams <hello@petewritescode.com>
2023-12-15 21:51:09 +0000

    Add styling tokens and layout styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,8 @@
 import '@/styles/base.scss';
+import styles from './layout.module.scss';
+import clsx from 'clsx';
 import type { Metadata } from 'next';
+import { DM_Sans } from 'next/font/google';
 import { FC, ReactNode } from 'react';
 
 export type RootLayoutProps = {
@@ -10,8 +13,11 @@ export const metadata: Metadata = {
   title: 'Snapcat',
 };
 
+const dmSans = DM_Sans({ subsets: ['latin'], variable: '--font-dm-sans' });
+const classNames = clsx(styles.html, dmSans.variable);
+
 const RootLayout: FC<RootLayoutProps> = ({ children }) => (
-  <html lang="en">
+  <html lang="en" className={classNames}>
     <body>{children}</body>
   </html>
 );
