343f495703603d2047beac044ab5a814595b934b
Pete Williams <hello@petewritescode.com>
2024-02-02 15:10:01 +0000

    Make image type difference more explicit

--- src/api/utils/map-api-image-to-image.test.ts
+++ src/api/utils/map-api-image-to-image.test.ts
@@ -3,15 +3,22 @@ import { Image } from '@/types/image';
 import { mapApiImageToImage } from './map-api-image-to-image';
 
 describe('mapApiImageToImage', () => {
-  it('maps an API image to an image', () => {
-    const apiImage: ApiImage = {
+  it('maps an API image to an image, stripping unneeded additional properties', () => {
+    const apiImage = {
+      id: 'abc123',
+      url: 'https://image.com/cat.jpg',
+      width: 200,
+      height: 100,
+      sub_id: 'bcd234',
+      breeds: ['lorem', 'ipsum'],
+    } as ApiImage;
+
+    const image: Image = {
       id: 'abc123',
       url: 'https://image.com/cat.jpg',
       width: 200,
       height: 100,
     };
-
-    const image: Image = { ...apiImage };
 
     expect(mapApiImageToImage(apiImage)).toStrictEqual(image);
   });
--- src/api/utils/map-api-image-to-image.ts
+++ src/api/utils/map-api-image-to-image.ts
@@ -1,9 +1,16 @@
 import { ApiImage } from '@/types/api-image';
 import { Image } from '@/types/image';
 
-export const mapApiImageToImage = (apiImage: ApiImage): Image => ({
-  id: apiImage.id,
-  url: apiImage.url,
-  width: apiImage.width,
-  height: apiImage.height,
+// NOTE While this appears to just map objects 1:1, it serves the purpose of
+// stripping off additional properties from the API response.
+export const mapApiImageToImage = ({
+  id,
+  url,
+  width,
+  height,
+}: ApiImage): Image => ({
+  id,
+  url,
+  width,
+  height,
 });
